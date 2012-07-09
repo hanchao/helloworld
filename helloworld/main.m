43a7bc242297a1a34d1a36baefaa984c14f4a965
hanchao <hanchao0123@hotmail.com>
2012-07-09 20:04:37 +0800

    Initial Commit

---
+++ helloworld/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  helloworld
+//
+//  Created by 韩超 on 12-7-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
