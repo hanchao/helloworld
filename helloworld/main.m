b2e4a895a4721f068e51543a4b21e17fe015c554
hanchao <hanchao0123@hotmail.com>
2012-07-09 20:05:13 +0800

    and name

--- helloworld/main.m
+++ helloworld/main.m
@@ -14,7 +14,7 @@ int main(int argc, const char * argv[])
     @autoreleasepool {
         
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! hanchao");
         
     }
     return 0;
